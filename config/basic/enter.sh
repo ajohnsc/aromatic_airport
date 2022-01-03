0a6285244dc27572d0dea4319aaffb67f62d2353
AJ Canlas <ajscanlas@gmail.com>
2022-01-03 14:08:20 +0800

    Modified enter script for initial release
    Modified enter script for the following:
    - Created while loop for starting the VM
    - Connecting to the VM

--- config/basic/enter.sh
+++ config/basic/enter.sh
@@ -3,3 +3,14 @@
 # Created by: AJ Canlas
 # 
 
+source sysad.cfg
+cd $CONFIG_DIR/basic
+
+while [ `vagrant status | grep basic | grep running | wc -l` -eq `echo 0` ]
+do
+    printf "Starting basic box\t\t\t"
+    vagrant up &> /dev/null && success_output || error_output
+done
+
+echo "Connecting to basic box"
+vagrant ssh 
