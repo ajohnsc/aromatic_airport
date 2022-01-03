323bcdede5f28c7b883367ecc2bdcd991d267e14
AJ Canlas <ajscanlas@gmail.com>
2022-01-03 14:09:29 +0800

    Modified clean script for initial release
    Clean script contains:
    - Force destroy of basic VM

--- config/basic/clean.sh
+++ config/basic/clean.sh
@@ -3,3 +3,11 @@
 # Created by: AJ Canlas
 # 
 
+source sysad.cfg
+cd $CONFIG_DIR/basic
+
+while [ `vagrant status | grep basic | grep not | wc -l` -eq `echo 0` ]
+do
+    printf "Deleting basic box\t\t\t"
+    vagrant destroy -f &> /dev/null && success_output || error_output
+done
